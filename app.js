aa6ed6871db93db1cf7846df1bb362b28cf4a33b
Vinicius Zaniboni Chagas <viny.zanchagas@gmail.com>
2021-05-02 00:38:37 -0300

    Update app.js
    Adicionado bootstrap
    Adicionado navbar no layout principal (main do handlebars)
    Organizado pasta de código
    Organizado pasta de rotas
    Organizado pasta de views

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
     const handlebars = require('express-handlebars')
     const app = express()
     const admin = require('./routes/admin')
+    const path = require('path')
     //const mongoose = require('mongoose')
     
 //Configurações
@@ -12,10 +13,15 @@
     // Handlebars
         app.engine("handlebars", handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
-
+    //Mongoose
+        //Soon
+    
+    //Public
+        app.use(express.static(path.join(__dirname,"public")))
+        
 //Rotas
     app.use('/admin', admin)
-    
+
 //Outros
 const PORT = 8081
 app.listen(PORT, () => {
